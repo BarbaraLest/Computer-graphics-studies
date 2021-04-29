c0182a296f1183befb117cbfe72eeb2057df8e51
Barbara Rodrigues <barbara.rodri49@outlook.com>
2021-04-29 11:13:28 -0300

    part 1
    Transformadas, movimentação do mundo, display file, DDA, Bresenham (retas e circunferência), translação, rotação, escalonamento, transformada homogênea, reflexão, clipping, curvas (castejau, hermite, bezier, b-spline) de forma dividir para conquistar, polinomial/matricial e forward difference, 3D

---
+++ Projeto 1/uJanela.cpp
@@ -0,0 +1,15 @@
+//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "uJanela.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+Janela::Janela(double nxmin, double nymin, double nxmax, double nymax){
+  xmax = nxmax;
+  xmin = nxmin;
+  ymax = nymax;
+  ymin = nymin;
+}
+
